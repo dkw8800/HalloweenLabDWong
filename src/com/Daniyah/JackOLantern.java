121165d5838460eaca193543b644a0b819896ce6
BT_1N3_22@bktech.local <BT_1N3_22@bktech.local>
2018-10-26 11:39:32 -0400

    Initial commit

---
+++ src/com/Daniyah/JackOLantern.java
@@ -0,0 +1,28 @@
+package com.Daniyah;
+
+public class JackOLantern extends Runner{
+    public faceFeatures;
+
+    JackOLantern(java.lang.String[][]faceFeatures)
+    {
+        this.faceFeatures = faceFeatures;
+    }
+
+    public void edit(java.lang.String replace, int row, int column)
+    {
+
+    }
+
+    public java.lang.String toString()
+    {
+
+    }
+
+    public void fill(java.lang.String str)
+    {
+        for(int i=0;i<12;i++)
+        {
+            System.out.println(str + '\n');
+        }
+    }
+}
---
+++ src/com/Daniyah/Runner.java
@@ -0,0 +1,8 @@
+package com.Daniyah;
+
+public class Runner {
+
+    public static void main(String[] args) {
+	JackOLantern.fill(str);
+    }
+}
