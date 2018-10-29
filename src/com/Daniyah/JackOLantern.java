842095ff5326efcf6dd889ef92e2e69f850907a9
BT_1N3_25 <dwong0023@bths.edu>
2018-10-29 11:46:27 -0400

    changed

--- src/com/Daniyah/JackOLantern.java
+++ src/com/Daniyah/JackOLantern.java
@@ -1,28 +1,32 @@
 package com.Daniyah;
 
 public class JackOLantern extends Runner{
-    public faceFeatures;
+    public static String[][] faceFeatures;
 
     JackOLantern(java.lang.String[][]faceFeatures)
     {
         this.faceFeatures = faceFeatures;
     }
 
-    public void edit(java.lang.String replace, int row, int column)
+    public static void edit(java.lang.String replace, int row, int column)
     {
-
+        faceFeatures[row][column] = replace;
     }
+
 
     public java.lang.String toString()
     {
-
-    }
-
-    public void fill(java.lang.String str)
-    {
-        for(int i=0;i<12;i++)
+        for(int i=0;i<10;i++)
         {
             System.out.println(str + '\n');
+        }
+    }
+
+    public static void fill(java.lang.String str)
+    {
+        for(int i=0;i<faceFeatures.length;i++)
+        {
+            str = faceFeatures[i][i];
         }
     }
 }
